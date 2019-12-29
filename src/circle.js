83b5d0df48ce8a7ee4e202e5e567c7f1ef8b4a7f
Anil Kulkarni <anil.kulkarni@outlook.com>
2019-12-28 16:57:40 -0800

    Make sure that segments start and end on the same point, per the spec

--- src/circle.js
+++ src/circle.js
@@ -1,11 +1,15 @@
-import { NUMBER_OF_COORDINATES_IN_CIRCLE, RADIANS_BETWEEN_COORDINATES } from './constants'
+import {
+  NUMBER_OF_COORDINATES_IN_CIRCLE,
+  RADIANS_BETWEEN_COORDINATES,
+} from './constants';
 import getCoordinate from './get-coordinate';
 
 export default function circle({ center, radius }) {
-    const coordinates = [];
-    for (let i = 0; i < NUMBER_OF_COORDINATES_IN_CIRCLE; i++) {
-        const radian = i * RADIANS_BETWEEN_COORDINATES;
-        coordinates.push(getCoordinate({ center, radius, radian }));
-    }
-    return coordinates;
+  const firstCoordinate = getCoordinate({ center, radius, radian: 0 });
+  const coordinates = [];
+  for (let i = 1; i < NUMBER_OF_COORDINATES_IN_CIRCLE; i++) {
+    const radian = i * RADIANS_BETWEEN_COORDINATES;
+    coordinates.push(getCoordinate({ center, radius, radian }));
+  }
+  return [firstCoordinate, ...coordinates, firstCoordinate];
 }
--- src/cocentric-segment.js
+++ src/cocentric-segment.js
@@ -1,21 +1,49 @@
 import getCoordinate from './get-coordinate';
-import { NUMBER_OF_COORDINATES_IN_CIRCLE, RADIANS_BETWEEN_COORDINATES } from './constants'
+import {
+  NUMBER_OF_COORDINATES_IN_CIRCLE,
+  RADIANS_BETWEEN_COORDINATES,
+} from './constants';
 
-export default function cocentricSegment({ center, innerRadius, outerRadius, startRadian, endRadian }) {
-    let coordinates = [];
+export default function cocentricSegment({
+  center,
+  innerRadius,
+  outerRadius,
+  startRadian,
+  endRadian,
+}) {
+  const firstCoordinate = getCoordinate({
+    center,
+    radius: innerRadius,
+    radian: startRadian,
+  });
+  const coordinates = [];
 
-    // draw the inner circle
-    for (let radian = startRadian; radian < endRadian; radian += RADIANS_BETWEEN_COORDINATES) {
-        coordinates.push(getCoordinate({ center, radius: innerRadius, radian }));
-    }
-    coordinates.push(getCoordinate({ center, radius: innerRadius, radian: endRadian }));
+  // draw the inner circle
+  for (
+    let radian = startRadian + RADIANS_BETWEEN_COORDINATES;
+    radian < endRadian;
+    radian += RADIANS_BETWEEN_COORDINATES
+  ) {
+    coordinates.push(getCoordinate({ center, radius: innerRadius, radian }));
+  }
+  coordinates.push(
+    getCoordinate({ center, radius: innerRadius, radian: endRadian })
+  );
 
-    // the outer circle, starting from the outer angle to the inner angle
-    // that way we get the lines between the circles for free
-    for (let radian = endRadian; radian > startRadian; radian -= RADIANS_BETWEEN_COORDINATES) {
-        coordinates.push(getCoordinate({ center, radius: outerRadius, radian }))
-    }
-    coordinates.push(getCoordinate({ center, radius: outerRadius, radian: startRadian }));
-    coordinates.push(getCoordinate({ center, radius: innerRadius, radian: startRadian }));
-    return coordinates;
+  // the outer circle, starting from the outer angle to the inner angle
+  // that way we get the lines between the circles for free
+  for (
+    let radian = endRadian;
+    radian > startRadian;
+    radian -= RADIANS_BETWEEN_COORDINATES
+  ) {
+    coordinates.push(getCoordinate({ center, radius: outerRadius, radian }));
+  }
+  coordinates.push(
+    getCoordinate({ center, radius: outerRadius, radian: startRadian })
+  );
+  coordinates.push(
+    getCoordinate({ center, radius: innerRadius, radian: startRadian })
+  );
+  return [firstCoordinate, ...coordinates, firstCoordinate];
 }
